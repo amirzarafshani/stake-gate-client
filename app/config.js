cb079e647813ae72dae98d20f1fd46df26552201
amirzarafshani <amirzarafshani@gmail.com>
2022-07-26 10:20:55 +0300

    config file changed

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiUrl: 'http://192.168.1.23:3000/',
-  uploadUrl: 'http://192.168.1.23:3000',
+  apiUrl: 'http://206.189.49.113:3000/',
+  uploadUrl: 'http://206.189.49.113:3000',
   socketUrl: 'ws://192.168.1.23:3000/ws',
 };
