a4dd1d7117167a4a86b7c82317c8ecd147f7a4d5
amirzarafshani <amirzarafshani@gmail.com>
2022-08-29 19:49:02 +0300

    config file

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 export default {
-  platformUrl: 'http://206.189.49.113/',
-  apiUrl: 'http://206.189.49.113:3000/',
-  uploadUrl: 'http://206.189.49.113:3000'
+  platformUrl: 'http://api.diamond-stake.com/',
+  apiUrl: 'http://api.diamond-stake.com/',
+  uploadUrl: 'http://api.diamond-stake.com',
 };
